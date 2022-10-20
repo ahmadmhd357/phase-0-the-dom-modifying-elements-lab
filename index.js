bac605fb5e2fc7c74eb268570d1be22431f61599
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-21 01:33:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const body = document.querySelector('body')
+const main = document.getElementById('main')
+body.removeChild(main);
+const  newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "AHMAD is the champion"
